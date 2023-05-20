2ab9fcc7ed25fe9f975c7a9d8f54087019635088
Abayomi Mustapha <mustaphaabayomi71@gmail.com>
2023-05-20 14:08:11 +0100

    Make menu close on tab click in mobile view

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -207,6 +207,7 @@ const Header = () => {
                 return (
                   <li key={index}>
                     <NavLink
+                      onClick={() => setOpenMenu(false)}
                       to={element.path}
                       className={`block py-2 pl-3 pr-4 text-gray-900 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${
                         darkMode ? "darkNavLink" : "lightNavLink"
